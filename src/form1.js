06c7e39b22b40dbe631013cd3cb5ce54d4b2c9d9
unknown <tariq047@gmail.com>
2022-09-25 00:54:27 +0100

    more form updates

--- src/form1.js
+++ src/form1.js
@@ -1,15 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("loaded");
-        document.querySelector("form#responseForm").addEventListener("submit", function(e) {
+        document.querySelector("form").addEventListener("submit", function(e) {
             e.preventDefault();
-            for (i = 0; i<100; i++) {
-                console.log(i);
-            };
-        });
-    // document.getElementById("responseForm").addEventListener('submit', function(e) {
-    //     console.log("form submitted");
-    //     e.preventDefault();
-    //     let x = document.querySelector('form.responseForm').elements;
-    //     console.log("Username");
-    // });
+            const dataOne = document.querySelector("#projectInput").data;
+            console.log(dataOne);
+    });
 });

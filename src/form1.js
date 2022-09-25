cdd3401e89db26d7635dee96f9f10399a5fd2937
unknown <tariq047@gmail.com>
2022-09-25 02:02:01 +0100

    reset form on submit

--- src/form1.js
+++ src/form1.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("loaded");
         document.querySelector("form").addEventListener("submit", function(e) {
             e.preventDefault();
-            const dataOne = document.querySelector("#projectInput").data;
-            console.log(dataOne);
+            const a = document.getElementById("progressInput").value;
+            document.querySelector("form").reset();
     });
 });
